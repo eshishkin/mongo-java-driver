f39eb2f84d292d53f11b5507f6b45ff521c4eef5
Eugene Shishkin <shishkin.john@gmail.com>
2021-07-02 19:57:10 +0400

    Additional tests for CodecCache#putIfMissing

--- bson/src/test/unit/org/bson/internal/CodecCacheSpecification.groovy
+++ bson/src/test/unit/org/bson/internal/CodecCacheSpecification.groovy
@@ -16,6 +16,7 @@
 
 package org.bson.internal
 
+import org.bson.codecs.BooleanCodec
 import org.bson.codecs.MinKeyCodec
 import org.bson.codecs.configuration.CodecConfigurationException
 import org.bson.types.MinKey
@@ -40,5 +41,47 @@ class CodecCacheSpecification extends Specification {
 
         then:
         thrown(CodecConfigurationException)
+    }
+
+    def 'should throw if cached codec for class is null'() {
+        when:
+        def cache = new CodecCache()
+        cache.put(MinKey, null)
+        cache.getOrThrow(MinKey)
+
+        then:
+        thrown(CodecConfigurationException)
+    }
+
+    def 'should put and return the cached codec if there was no coded in cache'() {
+        when:
+        def codec = new MinKeyCodec()
+        def cache = new CodecCache()
+        cache.putIfMissing(MinKey, codec)
+
+        then:
+        cache.getOrThrow(MinKey).is(codec)
+    }
+
+    def 'should return already cached codec if there was coded in cache'() {
+        when:
+        def codec = new BooleanCodec()
+        def cache = new CodecCache()
+        cache.put(MinKey, codec)
+        cache.putIfMissing(MinKey, new MinKeyCodec())
+
+        then:
+        cache.getOrThrow(MinKey).is(codec)
+    }
+
+    def 'should put and return new cached codec if there was coded in cache but it was null'() {
+        when:
+        def codec = new MinKeyCodec()
+        def cache = new CodecCache()
+        cache.put(MinKey, null)
+        cache.putIfMissing(MinKey, codec)
+
+        then:
+        cache.getOrThrow(MinKey).is(codec)
     }
 }
